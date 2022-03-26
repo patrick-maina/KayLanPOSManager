d247a121718586e43d6c7b35731bf149118be3ca
Patrick Maina <patnjoclem@yahoo.com>
2022-03-26 18:04:39 +0300

    Registering DbTables
    Created Tables for the App

---
+++ KayLanPOSData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+( 
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY,
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
