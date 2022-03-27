fbc6a4d43f158467b36f8feeb3291fdb663b7abf
Patrick Maina <patnjoclem@yahoo.com>
2022-03-27 11:54:08 +0300

    Login Wire Up
    Login Form n API Connected and working

---
+++ KayLanPOSDesktopUI/Helper/IApiHelper.cs
@@ -0,0 +1,10 @@
+﻿using KayLanPOSDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace KayLanPOSDesktopUI.Helper
+{
+    public interface IApiHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string userName, string password);
+    }
+}
